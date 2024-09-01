40363d7752d6a41687968056deba83b3b8e0dfa9
András Somi <somiandras@gmail.com>
2024-09-01 15:51:15 +0200

    initial setup

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::{Duration, Timer};
+use panic_probe as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    defmt::info!("Initializing...");
+
+    let peripherals = embassy_rp::init(Default::default());
+    let mut led = Output::new(peripherals.PIN_25, Level::Low);
+
+    defmt::info!("Initialized.");
+
+    loop {
+        defmt::info!("LED on!");
+        led.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+
+        defmt::info!("LED off!");
+        led.set_low();
+        Timer::after(Duration::from_millis(50)).await;
+    }
+}
